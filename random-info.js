1ff55f2fb268ea622aaa466a0109051c560345ce
woods2sr <woods2sr@mail.uc.edu>
2025-03-28 23:25:12 -0400

    made a click me button

--- random-info.js
+++ random-info.js
@@ -1,9 +1,17 @@
 // List of possible choices
-
+function showInfo() {
+   var inforList = [
+      "Ivan",
+      "Cookie",
+      "Vlad",
+      "<h2>Hello world!</h2>"
+   
+   ];
 
    // Generate a random number and pick that item from the array
-
+   var randomizer = Math.floor(Math.random() * inforList.length);
    
    // Write that information in the HTML element that has the ID of "info"
-
+   document.getElementById("info").innerHTML = infoList[randomizer];
    
+}
